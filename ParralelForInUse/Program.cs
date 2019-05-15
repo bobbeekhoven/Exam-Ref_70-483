7f733518fd145e3b8d5a0748538493d476895205
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2019-05-15 12:57:47 +0200

    Add files via upload

---
+++ ParralelForInUse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParralelForInUse
+{
+    class Program
+    {
+        static void WorkOnItem(int item)
+        {
+            Console.WriteLine("Started working on: " + item);
+            Thread.Sleep(100);
+            Console.WriteLine("Finished working on: " + item);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] items = Enumerable.Range(0, 500).ToArray();
+
+            Parallel.For(0, items.Length, i =>
+            {
+                WorkOnItem(items[i]);
+            });
+            Console.WriteLine("Finished processing. Press a key to end");
+            Console.ReadKey();
+        }
+    }
+}
