0ddca37035b1da84b6d795e30230926f84c88cd4
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2019-05-17 14:19:40 +0200

    Add files via upload

---
+++ Create-RunTasks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Create_RunTasks
+{
+    class Program
+    {
+        public static void DoWork()
+        {
+            Console.WriteLine("Work starting");
+            Thread.Sleep(2000);
+            Console.WriteLine("Work finished. Press a key to end.");
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Task newTask = new Task(() => DoWork());
+            newTask.Start();
+            newTask.Wait();
+        }
+    }
+}
