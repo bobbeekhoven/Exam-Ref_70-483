996a1a2a1c7d5ad845f09f0781ea0fa794756785
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2019-05-24 13:11:55 +0200

    Add files via upload

---
+++ SharedFlagVariable/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace SharedFlagVariable // Listing 1-24
+{
+    class Program
+    {
+        static bool tickRunning; // Flag variable
+        static void Main(string[] args)
+        {
+            tickRunning = true;
+
+            Thread tickThread = new Thread(() =>
+            {
+                while (tickRunning)
+                {
+                    Console.WriteLine("Tick");
+                    Thread.Sleep(1000);
+                }
+            });
+
+            tickThread.Start();
+
+            Console.WriteLine("Press a key to stop the clock");
+            Console.ReadKey();
+            tickRunning = false;
+            Console.WriteLine("Press a key to exit");
+
+            // Listing 1-25
+            Thread threadToWaitFor = new Thread(() =>
+            {
+                Console.WriteLine("Thread starting");
+                Thread.Sleep(2000);
+                Console.WriteLine("Thread done");
+            });
+
+            threadToWaitFor.Start();
+            Console.WriteLine("Joining thread");
+            threadToWaitFor.Join();
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+
+    }
+}
