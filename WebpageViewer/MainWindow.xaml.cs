1c3923d951a9386f74803bc514afed0a909b86ca
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2019-06-06 13:36:28 +0200

    Add files via upload

---
+++ WebpageViewer/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WebpageViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async Task<string> FetchWebPage(string url)
+        {
+            HttpClient httpclient = new HttpClient();
+            return await httpclient.GetStringAsync(url);
+        }
+
+        // awaiting parallel tasks
+        //static async Task<IEnumerable<string>> FetchWebPages(string [] urls)
+        //{
+        //    var tasks = new List<Task<string>>();
+
+        //    foreach (string url in urls)
+        //    {
+        //        tasks.Add(FetchWebPages(url));
+        //    }
+
+        //    return await Task.WhenAll(tasks);
+        //}
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ResultTextBlock.Text = await FetchWebPage(URLTextBox.Text);
+                StatusTextBlock.Text = "Page Loaded";
+            }
+            catch (Exception ex)
+            {
+                StatusTextBlock.Text = ex.Message;
+            }
+        }
+    }
+}
